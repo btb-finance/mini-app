4fb850c007d1d7ec76176b2c72d16c4f4de40a36
Olivia Mikayla <OliviaMikaylaa@gmail.com>
2025-04-25 05:37:19 +0200

    Update NFT contract address and reduce NFT price from 10000 to 1000 BTB

--- src/app/nft/constants.ts
+++ src/app/nft/constants.ts
@@ -1,11 +1,11 @@
 // NFT contract address
-export const NFT_CONTRACT_ADDRESS = "0xB04cEb91Bb895B60BBf485aEBbf58A2B77b8BBaB";
+export const NFT_CONTRACT_ADDRESS = "0x000081733751860A8E5BA00FdCF7000b53E90dDD";
 
 // BTB Token contract address (for NFT purchases)
 export const BTB_TOKEN_ADDRESS = "0xBBF88F780072F5141dE94E0A711bD2ad2c1f83BB";
 
 // Default values
-export const NFT_PRICE_BTB = 10000;
+export const NFT_PRICE_BTB = 1000;
 export const DEFAULT_NFT_COUNT = "1";
 
 // For backward compatibility
